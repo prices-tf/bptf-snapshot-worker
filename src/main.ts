c83967b81981d89b4813e79d974faaaa7b7a5f8c
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-09-14 23:44:20 +0200

    chore: remove unused import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { Config } from './common/config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
