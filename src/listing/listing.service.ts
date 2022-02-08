fc0d671c0b581c7c1258547f6279360bf5b116cc
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-02-08 19:08:25 +0100

    fix: use new domain
    backpack.tf is unavailable and new domain is backpacktf.trade

--- src/listing/listing.service.ts
+++ src/listing/listing.service.ts
@@ -51,7 +51,7 @@ export class ListingService {
 
     return this.httpService
       .get<ClassifiedsSearchResponse>(
-        'https://backpack.tf/api/classifieds/listings/snapshot',
+        'https://backpacktf.trade/api/classifieds/listings/snapshot',
         {
           params: qs,
         },
